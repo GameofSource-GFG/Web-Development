4f0991b4208b9d82c25d537d8ca3fc8511d58f40
harshal0902 <harshalraikwar07.com>
2020-10-21 18:44:16 +0530

    relevant message

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faLightbulb, faTrophy } from '@fortawesome/free-solid-svg-icons'
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>GameOfSource</h1>
-      </header>
+    <div className="app">
     </div>
   );
 }
