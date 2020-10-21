fe208c3cc4b0da285f19d228c90214a403f6a1a0
Somashri Rastogi <somashrirastogi789@gmail.com>
2020-10-22 02:08:00 +0530

    Contact Us page created

---
+++ client/src/component/Contact-Us/ContactUs.js
@@ -0,0 +1,29 @@
+import React, { Component} from 'react';
+
+class ContactUs extends Component{
+render() {
+  return (
+    <div className="App">
+    <p>Contact Us</p>
+    <div >
+    <form >
+    <label>Name </label>
+    <input type="text" id="fname" name="yName" placeholder="Your name.."/>
+    <label >Contact no.</label>
+    <input type="text" id="cno." name="Phone no." placeholder="Your contact no." />
+
+
+    <label>Email</label>
+    <input type="email" id="email" name="email" placeholder="Your email" />
+
+
+    <label>Your Message</label>
+    <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+    <input type="submit" value="Submit" />
+    </form>
+    </div>
+    </div>
+  );
+}
+}
+export default ContactUs;
