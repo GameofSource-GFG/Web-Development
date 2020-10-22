7ab46c31afaef87979a358f10865c70122714b46
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2020-10-22 18:23:00 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AboutUs />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
