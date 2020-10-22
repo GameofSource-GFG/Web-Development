c974e50c0d7a92ab53c203828b13e6b713b67580
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2020-10-22 18:56:40 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import AboutUs from './AboutUs';
-import './Aboutus.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
