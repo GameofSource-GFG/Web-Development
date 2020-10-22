77509f2c6760e051129729458f6661707cfce979
Priyanshu <Priyanshukaushal878@gmail.com>
2020-10-22 14:40:57 +0530

    Responsive Timelineupdated

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Timeline from './components/timeline';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
