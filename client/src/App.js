67aa137c0ac7d5b6f032d68d3cdb1317adbfbfcc
tejaswika <teju050301@gmail.com>
2020-10-22 09:17:53 +0530

    change

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import "./committee.css";
-import Committee from "./component/CommitteeMembers/Committee";
+import Committee from "./component/CommitteeMembers/committee";
 
 function App() {
   return (
