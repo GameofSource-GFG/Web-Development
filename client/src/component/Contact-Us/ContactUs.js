3b35946497d2939299e768c239657e18104366d5
Somashri rastogi <65566421+somashrirastogi@users.noreply.github.com>
2020-10-22 20:53:56 +0530

    update

--- client/src/component/Contact-Us/ContactUs.js
+++ client/src/component/Contact-Us/ContactUs.js
@@ -3,26 +3,31 @@ import React, { Component} from 'react';
 class ContactUs extends Component{
 render() {
   return (
+    <body className="adder">
     <div className="field">
-    <p>Contact Us</p>
-    <div >
-    <form>
-    <label>Name </label>
-    <input type="text" id="fname" name="yName" placeholder="Your name.."/>
+    <p className="para">Contact Us</p>
+    <div className="ev">
+    <form className="forms">
+    <label >Name </label>
+
+    <input type="text" id="fname" name="yName" placeholder="Your name.." className="name"/>
     <label >Contact no.</label>
-    <input type="text" id="cno." name="Phone no." placeholder="Your contact no." />
+
+    <input type="text" id="cno." name="Phone no." placeholder="Your contact no." className="no" />
 
 
-    <label>Email</label>
-    <input type="email" id="email" name="email" placeholder="Your email" />
+    <label >Email</label>
+
+    <input type="email" id="email" name="email" placeholder="Your email" className="mail"/>
 
 
-    <label>Your Message</label>
-    <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
-    <input type="submit" value="Submit" />
+    <label >Your Message</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." className="msg"></textarea>
+    <input type="submit" value="Submit" className="sub" />
     </form>
     </div>
     </div>
+    </body>
   );
 }
 }
