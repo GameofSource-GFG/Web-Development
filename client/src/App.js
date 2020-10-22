a9c16cb1a470effe23eb3c9701f7911b443bec3b
Somashri rastogi <65566421+somashrirastogi@users.noreply.github.com>
2020-10-22 21:15:14 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React from 'react';	
 
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg';	
+import './App.css';	
 
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-     
-       </div>
-  );
+function App() {	
+  return (	  
+    <div className="App">	    
+      <header className="App-header">	     
+        <img src={logo} className="App-logo" alt="logo" />	     
+        <h1>GameOfSource</h1>	
+      </header>	
+    </div>	
+  );	  
 }
-
-export default App;
