b995587bc07bbc09d395454f29fa25cb9af1cc79
Ashish Chawda <56932286+pixan198@users.noreply.github.com>
2020-10-23 09:38:39 +0000

    prettify code

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';	
-import logo from './logo.svg';	
-import './App.css';
-import AboutUs from './components/AboutUs/AboutUs';
-import Commiittee from './components/CommitteeMembers/committee';
-import Homepage from './components/Homepage/Homepage';
-import Timeline from './components/Timeline/timeline';
-import ContactUs from './components/Contact-Us/ContactUs';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Commiittee from "./components/CommitteeMembers/committee";
+import Homepage from "./components/Homepage/Homepage";
+import Timeline from "./components/Timeline/timeline";
+import ContactUs from "./components/Contact-Us/ContactUs";
 
-function App() {	
-  return (	  
-    <>	    
-    <Homepage/>
-    </>	
-  );	  
+function App() {
+  return (
+    <>
+      <Homepage />
+    </>
+  );
 }
 export default App;
