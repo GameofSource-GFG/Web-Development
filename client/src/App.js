1ec1c467da749752ffc6f20689361568aad3803c
harshal0902 <harshalraikwar07.com>
2020-10-23 22:51:49 +0530

    relevant message

--- client/src/App.js
+++ client/src/App.js
@@ -6,33 +6,32 @@ class App extends Component {
 
     componentDidMount() {
         this.renderMap()
-        // this.getVenues()
       }
-
+//Please enter your API key below to view the map
     renderMap = () => {
-        loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyD1DrDBUd6GNL2EIBCxK-K0OjkTny8kbuA&callback=initMap")
+        loadScript("https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEYA&callback=initMap")
         window.initMap = this.initMap
       }
-
-      // getVenues = () => {
-      //   const endPoint = "https://api.foursquare.com/v2/venues/explore?"
-      //   const parameters = {
-      //     client_id: "PMHC2WA1VCBHVYOPPSJ0QSBYTLRF4PNJ04OWVWV0PZJ0QFIR",
-      //     client_secret: "CULSZZ44YAEBOWBFGPB4BF5ISRXXSNYR0EE3JV3CNE2ZWHV0",
-      //     query: "Campus",
-      //     near: "Chennai",
-      //     v: "20182507"
-      //   }
+// Please enter your client_id and client_secret
+      getVenues = () => {
+        const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+        const parameters = {
+          client_id: "",
+          client_secret: "",
+          query: "Campus",
+          near: "Chennai",
+          v: "20182507"
+        }
       
 
-      // axios.get(endPoint + new URLSearchParams(parameters))
-      // .then(response => {
-      //     console.log(response.data.response.groups[0].items)
-      // })
-      // .catch(error => {
-      //   console.log("ERROR!! " + error)
-      // }) 
-
+      axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+          console.log(response.data.response.groups[0].items)
+      })
+      .catch(error => {
+        console.log("ERROR!! " + error)
+      }) 
+    }
     initMap = () => {
         var map = new window.google.maps.Map(document.getElementById('map'), {
             center: {lat: 12.8231, lng: 80.0442},
