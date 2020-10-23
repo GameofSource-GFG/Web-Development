69a1c3e692752af5ab75e247cd421a72f55f38e2
Priyanshu <Priyanshukaushal878@gmail.com>
2020-10-24 03:14:56 +0530

    Added Google Firebase Authentication setup#33

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+
+// Add here Var config from firebase console-> console.firebase.google.com
+var config={
+  apikey:" ",
+  authDomain:" ",
+  databaseURL:" ",
+  storageBucket:" ",
+};
+
+//Initialize App Config
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
