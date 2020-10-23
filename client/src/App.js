57dc099ffc462147bb29cd1381fc534b4d1ab3ef
sachin <sachinsom507@gmail.com>
2020-10-22 21:23:04 -0700

    additional changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>GameOfSource</h1>
