447a0ffaf2ba409896ed2edecb7f29bd9e781945
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2020-10-25 00:27:25 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Map from "./components/Map/Map";
-
 
 function App() {
   return (
     <>
-  <div className="app">
+  
     </div>
     </>
   );
