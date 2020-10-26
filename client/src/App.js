3d1b657627b0b577aefc4fff70dc57398f70042a
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2020-10-26 22:12:12 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import Commiittee from "./components/CommitteeMembers/committee";
 import Homepage from "./components/Homepage/Homepage";
 import Timeline from "./components/Timeline/timeline";
 import ContactUs from "./components/Contact-Us/ContactUs";
-import MisVis from "./components/Mission&Vision/MisVis";
 
 function App() {
   return (
