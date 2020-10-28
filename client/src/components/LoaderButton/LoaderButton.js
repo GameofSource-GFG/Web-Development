91118fa376dc2df679c88d1342e2eadf3ce4e0d2
Shubhadarshie Nanda <50710277+nanda-mik@users.noreply.github.com>
2020-10-28 20:41:55 +0530

    refact

--- client/src/components/LoaderButton/LoaderButton.js
+++ client/src/components/LoaderButton/LoaderButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button} from "react-bootstrap";
 import Spinner from 'react-bootstrap/Spinner'
-import "./LoaderButton.css";
+import "./Loaderbutton.css";
 
 export default function LoaderButton({
   isLoading,
