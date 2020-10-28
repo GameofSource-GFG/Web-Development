64f85c44ef5b8c6676626d79198c3230c5b07981
Shubhadarshie Nanda <50710277+nanda-mik@users.noreply.github.com>
2020-10-28 20:25:28 +0530

    resolved conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
 import AboutUs from "./components/AboutUs/AboutUs";
 import Commiittee from "./components/CommitteeMembers/committee";
 import Homepage from "./components/Homepage/Homepage";
 import Timeline from "./components/Timeline/timeline";
 import ContactUs from "./components/Contact-Us/ContactUs";
-import SocialMediaHandles from './components/SocialMediaHandles'
 import { FooterContainer } from './footer_container/footer';
 
 function App() {
   return (
     <>
       <Homepage />
-
+      <FooterContainer />
     </>
-
   );
 }
 export default App;
