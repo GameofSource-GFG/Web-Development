3bdf27f0db358bd9007e0dc60b7b36d038d4dd26
harshal0902 <harshalraikwar07.com>
2020-10-28 17:31:38 +0530

    relevant message

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,20 @@ import Commiittee from "./components/CommitteeMembers/committee";
 import Homepage from "./components/Homepage/Homepage";
 import Timeline from "./components/Timeline/timeline";
 import ContactUs from "./components/Contact-Us/ContactUs";
+// import React, { useEffect } from 'react';
+// import ReactGA from 'react-ga';
 
 function App() {
+// // Enter your Google Analytics tracking ID
+//   useEffect(() => {
+//     ReactGA.initialize('UA-');
+//     // For reporting page viewers 
+//     ReactGA.pageview(window.location.pathname + window.location.search);
+//   }, [])
+
+//   useEffect(() => {
+//    console.log(window.location.pathname)
+//   })
   return (
     <>
       <Homepage />
