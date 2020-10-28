1617b4b85f7d70b0cdd864d9f6f49bee77b31786
Priyanshu <Priyanshukaushal878@gmail.com>
2020-10-28 12:36:49 +0530

    fixed confilict

--- client/src/components/Navbar/RightNav.jsx
+++ client/src/components/Navbar/RightNav.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-
 const StyledRightNav = styled.ul`
         width: 100vh;
         height: 10vh;
--- client/src/components/routes.js
+++ client/src/components/routes.js
@@ -4,10 +4,8 @@ import { AboutUs } from './AboutUs/AboutUs';
 import { Events } from '';
 import { ContactUs } from './Contact-Us/ContactUs';
 import { MemberBlock } from './CommitteeMembers/MemberBlock';
-
 import { NavBar } from './Navbar/RightNav';
 import { Route, Switch, Redirect } from 'react-router-dom';
-
 export function Routes() {
     return (
         <div>
