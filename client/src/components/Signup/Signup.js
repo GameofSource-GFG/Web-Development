af7c8ddbfced745711f5b2c41b42dc047374a07c
amisha64 <amishakirti6410@gmail.com>
2020-10-28 19:38:19 +0530

    Password validator added

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -13,6 +13,7 @@ import { onError } from "../../libs/errorLib";
 import "./Signup.css";
 
 export default function Signup() {
+  
   const [fields, handleFieldChange] = useFormFields({
     email: "",
     password: "",
@@ -24,9 +25,25 @@ export default function Signup() {
   //const { userHasAuthenticated } = useAppContext();
   const [isLoading, setIsLoading] = useState(false);
 
+  var passwordValidator = require('password-validator');
+
+// Create a schema
+var schema = new passwordValidator();
+
+// Add properties to it
+schema
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits(2)                                // Must have at least 2 digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist these values
+
   function validateForm() {
     return (
       fields.email.length > 0 &&
+      schema.validate(fields.password)&&
       fields.password.length > 0 &&
       fields.password === fields.confirmPassword
     );
@@ -92,6 +109,7 @@ export default function Signup() {
         </FormGroup>
         <FormGroup controlId="password" bsSize="large">
           <FormLabel>Password</FormLabel>
+          <Form.Text>Your password should be between 8 to 100 characters.It must contain a lowercase and uppercase character and 2 numeric digits.</Form.Text>
           <FormControl
             type="password"
             value={fields.password}
