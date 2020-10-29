fafcdc9802594e52fb0b4f832d54409ac20c781d
sachin <sachinsom507@gmail.com>
2020-10-29 08:35:06 -0700

    additional changes

--- client/src/components/firebase.js
+++ client/src/components/firebase.js
@@ -16,20 +16,20 @@ var config={
  
   firebase.initializeApp(config);
 
-  // //get elements
-  // const txtEmail = document.getElementById('txtEmail');
-  // const txtPassword = document.getElementById('txtPassword');
-  // const btnLogin = document.getElementById('btnLogin');
+  //get elements
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLogin = document.getElementById('btnLogin');
  
-  // // Add login event
-  // btnLogin.addEventListener('click', e=>{
-  //   // get email and pass
-  //   const email = txtEmail.value;
-  //   const pass = txtPassword.value;
-  //   const auth = firebase.auth();
+  // Add login event
+  btnLogin.addEventListener('click', e=>{
+    // get email and pass
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
   
-  //   //  Log in
-  //   auth.signInWithEmailAndPassword(email,pass);
-  // })
+    //  Log in
+    auth.signInWithEmailAndPassword(email,pass);
+  })
 
   export default firebase;
